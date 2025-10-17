e15191b7775bcb0b56516b2824379b94f74bd30d
saepudinasep <saepudinasep2001@gmail.com>
2025-10-17 09:53:24 +0700

    [UPDATE] Merubah tampilan about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,7 @@ export default function About() {
         <div className="p-10 text-center">
             <h1 className="text-3xl font-bold text-indigo-400">About Page</h1>
             <p className="mt-4 text-gray-300">
-                Ini adalah proyek contoh React + Tailwind + Vite.
+                Ini adalah halaman About
             </p>
         </div>
     );
